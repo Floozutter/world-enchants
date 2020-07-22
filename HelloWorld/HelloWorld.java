495952e6233123e5aecc16cb5c4cd5ec9080bd22
Floozutter <floozutter@gmail.com>
2020-07-21 19:45:45 -0700

    Add HelloWorld

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HelloWorld extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		getLogger().info("Hello, World!");
+	}
+	@Override
+	public void onDisable() {
+		getLogger().info("Goodbye, World!");
+	}
+}
---
+++ HelloWorld/README.md
@@ -0,0 +1,9 @@
+# HelloWorld
+An example Spigot plugin.
+
+## Usage
+To create the JAR:
+```
+$ javac -classpath spigot-api-shaded.jar HelloWorld.java
+$ jar cfv HelloWorld.jar HelloWorld.class
+```
